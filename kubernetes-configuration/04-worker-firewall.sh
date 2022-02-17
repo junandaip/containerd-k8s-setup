fe3fc892ba6354670ce2ba372ef992c416293167
sovietoperatives <65005696+junandaip@users.noreply.github.com>
2022-02-18 01:30:35 +0700

    change everything

--- kubernetes-configuration/04-worker-firewall.sh
+++ kubernetes-configuration/04-worker-firewall.sh
@@ -2,6 +2,6 @@
 
 firewall-cmd --add-masquerade --permanent 
 firewall-cmd --add-port=179/tcp --permanent 
-    firewall-cmd --add-port 10250/tcp --permanent
-    firewall-cmd --add-port 30000-32767/tcp --permanent
-    firewall-cmd --reload
+firewall-cmd --add-port 10250/tcp --permanent
+firewall-cmd --add-port 30000-32767/tcp --permanent
+firewall-cmd --reload
