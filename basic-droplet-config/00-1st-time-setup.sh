643f4419653a66b00ffbb171ffa18633fd8a2b68
sovietoperatives <65005696+junandaip@users.noreply.github.com>
2022-02-18 00:31:40 +0700

    add hostname

--- basic-droplet-config/00-1st-time-setup.sh
+++ basic-droplet-config/00-1st-time-setup.sh
@@ -26,3 +26,15 @@ timedatectl set-timezone Asia/Jakarta
 yum install ntp -y 
 systemctl start ntpd
 systemctl enable ntpd 
+
+# Edit hostname
+cat << EOF >> /etc/hosts
+
+# hostname for private cluster
+10.104.0.7 bastion-host
+10.104.0.2 k8s-master-1 
+10.104.0.4 k8s-master-2
+10.104.0.6 k8s-master-3 
+10.104.0.3 k8s-worker-1
+10.104.0.5 k8s-worker-2
+EOF
